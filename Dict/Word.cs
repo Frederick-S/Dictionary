cec20d9c6b34ef23117b4437dd61cad0bf303f2f
Frederick-S <mao_xiaodan@hotmail.com>
2018-06-18 16:38:37 +0800

    Add style cop

---
+++ Dict/Word.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Dict
+{
+    public class Word
+    {
+        public string Name { get; set; }
+
+        public List<string> Explanations { get; set; }
+
+        public List<string> Suggestions { get; set; }
+    }
+}
